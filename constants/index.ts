3d85aea67d82801ed566e039afe9e1e6d0621c8c
David Cartwright <d.p.cartwright@gmail.com>
2023-03-21 20:23:01 +0000

    initial setup of project

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'AwfulCode Site'
+export const appDescription = 'Ramblings and gateway to hosted projects'
